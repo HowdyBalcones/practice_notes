70459f20b416cfcc6d1cb266fa4b1558d18d4a97
Select Graphics <selectgraphics@Selects-iMac.local>
2023-11-30 17:22:14 -0600

    C++ notes 1

---
+++ 004-CPLUSPLUS/practices/helloWorld.cpp
@@ -0,0 +1,7 @@
+#include "libs/std_lib_facilities.h"
+
+int main()
+{
+	cout<<"Push your chair back from the table.\nStand up.\nTurn to face the direction of the restrooms, located directly behind the desk for graphics, in the main office area.\nUse your legs and walk to the bathroom door.\nUse the door handle to open to first door to the restroom.\nRepeat this process for the second door in the small hallway to the bathroom.\nEnter the bathroom,\nNow you're on your own.\n"; //output Howdy Planet!
+	return 0;
+}
---
+++ 004-CPLUSPLUS/practices/helloWorld2.cpp
@@ -0,0 +1,7 @@
+#include "libs/std_lib_facilities.h"
+
+int main()
+{
+	cout<<"1. INGREDIENTS\n\nBlueberries, Butter, Flour, Vanilla, Eggs, Salt, Milk, Baking Powder, Sugar\n\nSTREUSEL TOPPING:\n\nCinnamon, Brown Sugar, Sugar, Flour, Salt, Butter\n2. INSTRUCTIONS\n\nA.  Preheat the oven to 400 degrees.\nA.1 Prepare a muffing pan with cupcake liners.\nA.2 In a large bowl, sift together 2 cups of flour, 1 cup of sugar, baking powder, and 1/2 teaspoon of salt.\nA.3 Set ingredients aside.\n\nB.0 In a medium bowl, whisk the eggs until smooth.\nB.1 Add the 1/2 cup of melted butter, milk, and vanilla, and whisk until combined.\nB.2 Add egg mixture to flour mixture and stir until combined. (Dough WILL be lumpy.)\nB.3 Fold in the blueberries\n";
+	return 0;
+}
