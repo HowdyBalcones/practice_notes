4d6443c87e29442a4f70d85d79e7c07a18372535
Camden Bailey <camdenbailey@Rhondas-iMac.local>
2024-11-19 16:25:27 -0600

    latest work on the project, working through the first method for importing into the contract object.

--- 006-illustrator_scripts/scripts/FRC-2.jsx
+++ 006-illustrator_scripts/scripts/FRC-2.jsx
@@ -1,5 +1,12 @@
+
 (function() {
-      
+   try {
+      // eslint-disable-next-line
+      #include "/Users/camdenbailey/Desktop/practice_notes/006-illustrator_scripts/scripts/FRC-obj.jsxinc";
+      #include "/Users/camdenbailey/Desktop/practice_notes/006-illustrator_scripts/scripts/002-data/FRC-data_tests.jsxinc";
+   }  catch(e) {
+      alert(e.name + ": " + e.message + "\nNo active document"); 
+   }    
    try {
       var doc = app.activeDocument;
    } catch(e) {
@@ -439,6 +446,207 @@ function renameGroup(newName) {
       alert(doc.selection[0].typename);
    }
 
+   function contains (list, target) {
+      for (var i = 0; i < list.length; ++i) {
+         if (list[i] == target) {
+            return true;
+         } 
+      }
+      return false;
+   } 
+
+   function match (list, target) {
+      for (var i = 0; i < list.length; ++i) {
+         if (list[i] == target) {
+            return list[i];
+         }
+      }
+      return false;
+   }
+
+   function index_of_section (list, target) {
+      for (var i = 0; i < list.length; ++i) {
+         if (list[i].section_name === target) {
+            return i;
+         } 
+      }
+      return false;
+   }
+
+   
+
+// ---- Object Functions ----
+   
+   function sheet_create_clean_contract (data) {
+        // call sheetJS and return simple aoa 
+   }
+   function import_contract (contract) {
+      // contract list --> will contain the entirety of the cleaned_contract and any contract revisions, newest at the end
+      // section list --> will be an aoo for each section with a section_name string and sign_list array.
+      // sign list --> will be an aoo of unique sign objects for the entire job, disregarding section keys
+      
+      if (contract instanceof Contract) {
+         contract.contract_list.push(contract.data);
+         contract.revision++; 
+      }
+      
+      var len = contract.contract_list.length;
+      var latest_revision = contract.contract_list[len - 1];
+      var unique_sections = [];
+      
+      for (var i = 0; i < latest_revision.length; ++i) {
+         var name = latest_revision[i][0];
+         if (!contains(unique_sections, name)) {
+            unique_sections.push(name);
+         }
+      } 
+
+      for (var i = 0; i < unique_sections.length; ++i) {
+         var section_name = unique_sections[i];
+         contract.section_list.push(new Section(section_name));
+      }
+      
+      // creates signs for each section in section_list
+      for (var i = 0; i < latest_revision.length; ++i) {
+         var item_section = latest_revision[i][0];
+         var item_key = latest_revision[i][1];
+         var item_count = latest_revision[i][2];
+         var item_description = latest_revision[i][3];
+         var matching_section_index = index_of_section(contract.section_list, item_section);
+         contract.section_list[matching_section_index].sign_list.push(new Sign(item_section, item_key, item_count, item_description)); 
+         contract.sign_list.push(new Sign(null, null, item_count, item_description));
+      }
+
+      // add duplicate sign objects together in the sign_list
+      for (var i = 0; i < contract.sign_list.length; ++i) {
+         var sign = contract.sign_list[i];
+         var sign_desc = sign.description;
+         var sign_count = sign.count;
+         var match_indexes = "";
+         // don't think this is working, bring home tonight! 
+         for (var j = 0; j < contract.sign_list.length; ++j) {
+            var sign_comparison = contract.sign_list[j];
+            if (sign_desc === sign_comparison.description) {
+               sign.count += sign_comparison.count;
+               // delete the matched sign
+               contract.sign_list.splice(j,1);
+               // match_indexes += j + " - ";
+            }
+         }
+         // alert(match_indexes);
+      }
+   }
+
+   function export_contract (contract) {
+      // your current contract object will be exported to csv or xlsx
+   }
+   function reorganize_contract () {
+      // check latest contract state with previous contract state
+      // if new state detected, then update the keys of the contract
+   }
+   function display_contract () {
+      // interaction between UI and contract object to display information to the user
+   }
+   function update_contract () {
+      // will change the contract object when the user alters and confirms the UI state
+   }
+   function add_revision (current_contract) {
+      // take a contract state and create  a revision copy of that state
+   }
+   function sign_create (target) {
+      // will take the currently se
+   }
+   function build_add (build_text) {
+     // take values from UI fields and create a build object
+   }
+   function build_edit (target_build) {
+      // change existing build object according to UI state
+   }
+   function build_sort (build_list) {
+      // checks the current contract build_list for duplicates and alerts the user before deleting
+   }
+   function label_add (label_name) {
+      // will create a new label based on UI state
+   }
+   function label_edit (target_label) {
+      // change the fields of an existing label
+   }
+   function label_create_from_section (section_list) {
+      // will create labels from the section table of contract object
+   }
+   function label_search (target, label_list) {
+      // search the label_list and return matched fields
+   }
+   function item_delete (target_object) {
+      // will search a list and delete the row based on a match
+   }
+   function ui_build (build_list) {
+      // this will be the code for user interface
+   }
+   function ui_label (label_list) {
+      // code for label interface
+   }
+
+   function obj_test (obj) {
+      if (obj instanceof Contract) {
+         alert(
+            obj.contract_name + "\n" +
+            obj.job_id + "\n" +
+            obj.contract_list[0][0][1] + "\n" + 
+            obj.section_list[1].section_name + "\n" +
+            obj.section_list[0].sign_list[0].description + "\n"
+         );
+      }
+   }
+
+   function contract_test_full (contract) {
+      var len = contract.contract_list.length;
+      var latest_revision = contract.contract_list[len - 1];
+      var test_contract_list = "";
+      var test_section_list = "";
+      var test_sign_list = "";
+      if (contract instanceof Contract) {
+         
+         for (var i = 0; i < latest_revision.length; ++i) {
+            var section = latest_revision[i][0];
+            var item_key = latest_revision[i][1];
+            var count = latest_revision[i][2];
+            var description = latest_revision[i][3];
+            test_contract_list += 
+               section + " - " +
+               item_key + " - " +
+               count + " - " +
+               description + "\n";
+         }
+
+         for (var i = 0; i < contract.section_list.length; ++i) {
+            var section = contract.section_list[i];
+            test_section_list += section.section_name + "\n";
+            for (var j = 0; j < section.sign_list.length; ++j) {
+               var sign = section.sign_list[j];
+               var key = sign.item_key;
+               var name = sign.description;
+               test_section_list += key + " - " + name + "\n";
+            }
+         }
+
+         for (var i = 0; i < contract.sign_list.length; ++i) {
+            var sign = contract.sign_list[i];
+            var sign_count = sign.count;
+            var sign_description = sign.description;
+            test_sign_list += sign_count + " - " + sign_description + "\n"
+         } 
+      }
+      alert(test_contract_list);
+      alert(test_section_list);
+      alert(test_sign_list);
+   }
+
+  var test_contract = new Contract("Jefferson Mars Inc.", "24RAM0123S", test_aoa);
+  import_contract(test_contract);
+//  obj_test(test_contract);
+  contract_test_full(test_contract);
+
 
 // ---- Function Testing ----
 // testingPrompt();
@@ -460,5 +668,5 @@ function renameGroup(newName) {
 //  setColor(doc.selection,[50,100,75,0]);
 // applyRandomly(doc.selection, setRandomRgb)
 // simpleTestEach(doc.selection);
-typeSizeIncrement();
+// typeSizeIncrement();
   })();
