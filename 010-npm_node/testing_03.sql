231df45f23bc29f3f1a3d981475e7be8125caf20
23340-WEBSTER <23340-webster@23340webster.lan>
2025-02-04 22:03:25 -0600

    postgres noodling

---
+++ 010-npm_node/testing_03.sql
@@ -0,0 +1,6 @@
+INSERT INTO cities VALUES (
+   'San Francisco', '(-194.0, 53.0)'
+--   'New York', '(-98.0, -100.1)',
+--   'Dallas', '(34.9, 69.9)',
+--   'San Francisco', '(56.7, 244.0)'
+);
---
+++ 010-npm_node/testing_04.sql
@@ -0,0 +1,2 @@
+INSERT INTO cities (name, location)
+   VALUES ('New York', '(67.8, 99.45)');
