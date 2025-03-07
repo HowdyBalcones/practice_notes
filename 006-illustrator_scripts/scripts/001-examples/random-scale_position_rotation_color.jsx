fa4053461d90416a413b56be454054bd2e60e3bf
Camden Bailey <camdenbailey@Rhondas-iMac.local>
2025-03-07 10:52:42 -0600

    Some illustrator scripting, random strokes

--- 006-illustrator_scripts/scripts/001-examples/random-scale_position_rotation_color.jsx
+++ 006-illustrator_scripts/scripts/001-examples/random-scale_position_rotation_color.jsx
@@ -33,6 +33,14 @@ function setFillColor(selectedItem) {
 	}
 }
 
+function random_rgb(min, max) {
+   var color = new RGBColor(); 
+   color.red = getRandomInt(getRandomInt(min, max), getRandomInt(min, max));
+   color.green = getRandomInt(getRandomInt(min, max), getRandomInt(min, max));
+   color.blue = getRandomInt(getRandomInt(min, max), getRandomInt(min, max));
+   return color;
+}
+
 function rotateItem(selectedItem) {
 	var angle = getRandomInt(0, 360); // random angle
 	selectedItem.rotate(angle);
@@ -41,6 +49,26 @@ function rotateItem(selectedItem) {
 function scaleItem(selectedItem) {
 	var scaleFactor = getRandomInt(50, 200); // random between 50% and 200%
 	selectedItem.resize(scaleFactor, scaleFactor); // uniform scaling
+}
+
+function random_stroke(selectedItem) {
+   var scaleFactor = getRandomInt(1, 15);
+   var randomColor = random_rgb(100, 150);
+   if (selectedItem.typename === "GroupItem") {
+      for (var i = 0; i < selectedItem.pageItems.length; ++i) {
+         var randomColor = random_rgb(100, 150);
+         var innerItem = selectedItem.pageItems[i];
+            random_stroke(innerItem);
+      }
+   } else if(selectedItem.typename === "CompoundPathItem") {
+      for (var i = 0; i < selectedItem.pathItems.length; ++i) {
+         var randomColor = random_rgb(100, 150);
+         var innerItem = selectedItem.pathItems[i];
+            random_stroke(innerItem);
+      }
+   } else {
+          selectedItem.strokeColor = randomColor;  
+   }
 }
 
 function repositionItem(selectedItem, artboard) {
@@ -55,18 +83,31 @@ function repositionItem(selectedItem, artboard) {
 	selectedItem.position = [newPositionX, newPositionY];
 }
 
+function process_selected(func_arr) {
+   for (var i = 0; i < selectedItems.length; ++i) {
+      var item = selectedItems[i];
+      for (var j = 0; j < func_arr.length; ++j) {
+         func_arr[j](item);
+      }
+   }
+}
+
 // check if anything is selected
 if (selectedItems.length > 0) {
 
 	for (var i = 0; i < selectedItems.length; i++) {
 		var selectedItem = selectedItems[i];
-		setFillColor(selectedItem);
-		rotateItem(selectedItem);
-		scaleItem(selectedItem);
-		repositionItem(selectedItem, artboard);
+		// setFillColor(selectedItem);
+		//rotateItem(selectedItem);
+		//scaleItem(selectedItem);
+		//repositionItem(selectedItem, artboard);
 	}
 
 } else {
 	alert("No items are selected!");
 }
+
+//process_selected([setFillColor, rotateItem]);
+//process_selected([random_stroke, setFillColor]);
+process_selected([random_stroke]);
 
