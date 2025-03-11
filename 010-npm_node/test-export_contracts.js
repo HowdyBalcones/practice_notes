13f5d6c1900f0f04abaccd3cf18ba10446932eb0
Camden Bailey <camdenbailey@Rhondas-iMac.local>
2025-03-11 14:03:56 -0500

    More XML parsing, left off at the race condition bug with export_contract

--- 010-npm_node/test-export_contracts.js
+++ 010-npm_node/test-export_contracts.js
@@ -190,6 +190,30 @@ function xml_file_name(data) {
    return file_name;
 }
 
+function build_key_obj(row) {
+   const section = row["0"].trim();
+   const key = row["1"];
+   const count = row["2"];
+   const description = row["3"];
+   const cost = row["4"];
+   const total_cost = row["2"] * row["4"];
+   const section_key = {"key": key, "section":section};
+   let key_obj = {
+      "section_list":[section_key],
+      "section_key": section_key,
+      "section": section,
+      "key": key,
+      "count": count,
+      "description": description,
+      "cost": cost,
+      "total_cost": total_cost,
+      set_total_cost() {
+         this.total_cost = this.cost * this.count;
+      }
+   }
+   return key_obj;
+}
+
 function build_xml_object(data) {
    const header_row = data[1];
    const data_row = data[2];
@@ -205,18 +229,9 @@ function build_xml_object(data) {
    let xml_sections = '';
    
    // this is what a row from the contract becomes, only includes essential cells but is flexible what can be included. 
-   function build_key_obj(row) {
-      let key_obj = {
-         "key": row["1"],
-         "count": row["2"],
-         "description": row["3"],
-         "cost": row["4"],
-         "total_cost": row["2"] * row["4"]
-      }
-      return key_obj;
-   }
+   
    try {
-   for (let i = 0; i < data.length; ++i) {
+   for (let i = 1; i < data.length; ++i) {
       let row = data[i];
       let section_name = row["0"].trim();
       if (!unique_sections.includes(section_name)) {
@@ -231,25 +246,26 @@ function build_xml_object(data) {
    // here we destructure the map object (contract body content) and serialize the data into xml
    const section_map_array = Array.from(section_map);
    for (let j = 0; j < section_map_array.length; ++j) {
-      let section_arr = section_map_array[j];
-      let section_name = section_arr[0];
-      let section_row_arr = section_arr[1];
-      xml_sections += serialize_sections(section_name, section_row_arr); 
+      let section_arr = section_map_array[j];            // is array
+      let section_name = section_arr[0].trim();          // is string
+      let section_row_arr = section_arr[1];              // is sub-array
+      let values_object = section_row_arr[0];            // is object
+      xml_sections += serialize_sections(escapeXML(section_name), section_row_arr); 
    }
 
+    const unique_signs_xml = `
+      ${serialize_unique_signs(unique_signs(data))}
+    `
    // metadata section serialized here 
    const metadata = `
-   <contract_num>${contract_num}</contract_num>
-   <contract_file>${contract_file}</contract_file>
-   <client_name>${client_name}</client_name>
-   <project_name>${project_name}</project_name>
+   <contract_num>${escapeXML(contract_num)}</contract_num>
+   <contract_file>${escapeXML(contract_file)}</contract_file>
+   <client_name>${escapeXML(client_name)}</client_name>
+   <project_name>${escapeXML(project_name)}</project_name>
    <project_address></project_address>
    <designer_name></designer_name>
    `
-   const unique_sign_list = `
-      <unique_sign_list>${escapeXML(unique_sign_list.join(","))}</unique_sign_list>
-   `
-   // content inside root is arranged here    
+  // content inside root is arranged here    
    xml_object = `
       <job_info>
       <contract>
@@ -259,36 +275,41 @@ function build_xml_object(data) {
       <metadata>
       ${metadata}
       </metadata>
+      <unique_sign_list>
+      ${unique_signs_xml}
+      </unique_sign_list>
       </job_info>
       `
-
+      
    } catch(err) {
       console.log(`Error serializing XML: ${err}`);
    }
    return xml_object;
 }
 
-const unique_signs = (data) => {
-   // actually somewhat challenging to visualize, 
-   // 1 - unique sign descriptions, with count and section data for each
-   // 2 - for each unique desc, push onto the map, increment count, add to section list
-   // 3 - return map 
-   let sign_list = new Map();
-   let unique_sign_obj = {
-      "section": '',
-      "count": '',
-      "desc": ''
-   }
+let unique_signs = (data) => {
+   let unique_sign_list = [];
+   
    for (i = 1; i < data.length; ++i) {
       let row = data[i];
-      let section = row["0"];
-      let count = row["2"];
-      let desc = row["3"];
-      if (sign_list.includes())
+      const new_obj = build_key_obj(row);
+      unique_sign_list = find_same_obj(unique_sign_list, new_obj);
    }
+   return unique_sign_list;
 }
 
-const escapeXML = (str) => {
+let find_same_obj = (arr, new_obj) => {
+   const existing_obj = arr.find(obj => obj.description === new_obj.description);
+   if (existing_obj) {
+      existing_obj.count += new_obj.count || 1;
+      existing_obj.set_total_cost();
+      existing_obj.section_list.push(new_obj.section_list[0]);
+   } else {
+      arr.push({...new_obj, count: new_obj.count || 1});
+   }
+   return arr;
+}
+let escapeXML = (str) => {
    if (!str) return "";
    return str.toString()
       .replace(/&/g, "&amp;")
@@ -301,23 +322,49 @@ const escapeXML = (str) => {
 
 function serialize_sections(section_name, items) {
    // expects an array of objects, key objects in this instance.
-   const itemsXML = items.map(item => `
-      <${escapeXML(item.key)}>
+   let itemsXML = items.map(item => `
+      <sign>
+         <key>${escapeXML(item.key)}</key>
          <description>${escapeXML(item.description)}</description>
          <count>${escapeXML(item.count)}</count>
          <cost>${escapeXML(item.cost)}</cost>
          <total_cost>${escapeXML(item.total_cost)}</total_cost>
-      </${escapeXML(item.key)}>
+      </sign>
       `).join("");
    
-   const section_info = `
+   let section_info = `
    <section>
-   <section_name>${escapeXML(section_name)}</section_name>
+   <section_name>${section_name}</section_name>
    ${itemsXML}
    </section>
    `;
 
    return section_info;
+}
+
+function serialize_unique_signs(unique_sign_arr) {
+   function section_list_xml(section_list) {
+      let list = section_list.map(section_key => `
+         <section_key>
+            <key>${escapeXML(section_key.key)}</key>
+            <section>${escapeXML(section_key.section)}</section>
+         </section_key>
+         `).join("");
+      return list;
+   };
+   
+   let unique_items_xml = unique_sign_arr.map(unique_sign => `
+      <unique_sign>
+         <section_list>
+            ${section_list_xml(unique_sign.section_list)}         
+         </section_list>
+         <description>${escapeXML(unique_sign.description)}</description>
+         <total_count>${escapeXML(unique_sign.count)}</total_count>
+         <each_cost>${escapeXML(unique_sign.cost)}</each_cost>
+         <total_cost>${escapeXML(unique_sign.total_cost)}</total_cost>
+      </unique_sign>
+      `).join(""); 
+   return unique_items_xml;
 }
 
 // this will be the function for cli interface 
